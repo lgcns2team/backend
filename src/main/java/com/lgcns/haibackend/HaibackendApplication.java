e08a08e46b93364bf48217d47fc068547c5ce72a
surimleeee <surimviaa@naver.com>
2025-11-27 11:06:12 +0900

    spring boot 3.4.12

---
+++ src/main/java/com/lgcns/haibackend/HaibackendApplication.java
@@ -0,0 +1,13 @@
+package com.lgcns.haibackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaibackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HaibackendApplication.class, args);
+	}
+
+}
