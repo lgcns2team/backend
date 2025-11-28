1815a0540e07e33f365d7cd7674206ad947417ca
hsuncho <hsuncho@naver.com>
2025-11-28 13:49:41 +0900

    feat: update db config

--- src/main/java/com/lgcns/haibackend/HaibackendApplication.java
+++ src/main/java/com/lgcns/haibackend/HaibackendApplication.java
@@ -2,12 +2,20 @@ package com.lgcns.haibackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class HaibackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HaibackendApplication.class, args);
-	}
+        Dotenv env = Dotenv.configure().ignoreIfMissing().load();
+        env.entries().forEach(entry ->
+            System.setProperty(entry.getKey(), entry.getValue())
+        );
+        SpringApplication.run(HaibackendApplication.class, args);
+    }
 
 }
