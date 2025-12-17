4bf6446a4c5fc176fdb25d48826efd707360de54
hsuncho <hsuncho@naver.com>
2025-12-17 15:43:37 +0900

    fix: 컬럼명 수정

--- src/main/resources/user_dummy_data.sql
+++ src/main/resources/user_dummy_data.sql
@@ -6,7 +6,7 @@ INSERT INTO users (
     nickname,
     password,
     classroom,
-    class_code
+    teacher_code
 ) VALUES (
     '11111111-1111-1111-1111-111111111111',
     1,
@@ -26,7 +26,7 @@ INSERT INTO users (
     nickname,
     password,
     classroom,
-    class_code
+    teacher_code
 ) VALUES
 (
     '22222222-2222-2222-2222-222222222222',
@@ -87,7 +87,7 @@ INSERT INTO users (
     nickname,
     password,
     classroom,
-    class_code
+    teacher_code
 ) VALUES (
     '44444444-4444-4444-4444-444444444444',
     2,
@@ -107,7 +107,7 @@ INSERT INTO users (
     nickname,
     password,
     classroom,
-    class_code
+    teacher_code
 ) VALUES
 (
     '55555555-5555-5555-5555-555555555555',
